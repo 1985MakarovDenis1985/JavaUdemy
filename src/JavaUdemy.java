0d465e644464b62c6f740c184492f02df1032f2e
Денис Макаров <denismakarov@MacBook-Pro-Denis.local>
2021-06-04 18:20:22 +0300

    lesson 5

---
+++ src/JavaUdemy.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+public class JavaUdemy {
+
+    public static void main(String[] args) {
+
+    }
+
+    String color;
+    String engine;
+    int speed;
+
+    int gaz(int spd){
+        speed += spd;
+        return speed;
+    };
+
+    int brakes(int brk){
+        speed -= brk;
+        return speed;
+    };
+
+    void show(){
+        System.out.println("color: " + color + " | engine: " + engine + " | speed: " + speed + "Mph");
+
+    }
+
+//    int sum(int a, int b, int c) {
+//        int res = a + b + c;
+//        return res;
+//    }
+
+// --- // --- // --- // --- function
+//    public static int[] readArr(){
+//        int[] arr;
+//        int num;
+//        Scanner in = new Scanner(System.in);
+//        num = in.nextInt();
+//        arr = new int[num];
+//    };
+
+//    public static int func(int a, int b) {
+//        int result = 1;
+//        for (int i = 0; i < b; i++)
+//            result *= a;
+//        return result;
+//    }
+    // ---- // --- // --- //
+
+
+    // ---- // --- // --- //
+
+//    public static void main(String[] args) {
+// ======================================= //
+
+//        // одномерный массив
+//        int[] numbers = new int[5];
+//        float[] floats = new float[]{1.1f, 2.2f, 3.3f};
+//
+//        // двумерный массив
+//        char[][] array = new char[3][];
+//        for(int i=0; i<array.length; i++){
+//            array[i] = new char[3];
+//        }
+
+
+// ===================================== //
+
+//        int[] arr;
+//        int num;
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Enter count of arr: ");
+//        num = in.nextInt();
+//        arr = new int[num];
+//        for(int i=0; i<num; i++){
+//            System.out.print("Enter number: ");
+//            arr[i] = in.nextInt();
+//        }
+//    }
+}
+
+class Car {
+    public static void main(String[] args) {
+        JavaUdemy car = new JavaUdemy();
+        car.color = "green";
+        car.engine = "V8";
+        car.speed = 254;
+        car.show();
+        car.gaz(56);
+        car.show();
+        car.brakes(231);
+        car.show();
+    }
+}
+
+
+//class Test {
+//    public static void main(String[] args) {
+//        JavaUdemy result = new JavaUdemy();
+//        int res = result.sum(1, 2, 3);
+//        System.out.println(res);
+//    }
+//}
